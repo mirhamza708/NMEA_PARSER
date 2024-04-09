d14220917b489682fd7056595eff6c991ae7bfa2
mirhamza708 <mirhamza708@gmail.com>
2024-04-09 14:37:25 +0500

    First commit containing only UART code which reads a NMEA sentence from GPS module.

---
+++ main/main.c
@@ -0,0 +1,9 @@
+/* 
+
+*/
+#include "uart.h"
+
+void app_main(void)
+{
+    uart_init();
+}
---
+++ main/uart.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "esp_log.h"
+#include <stdio.h>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "driver/uart.h"
+
+esp_err_t uart_init();
